6fa986918606ead158cb0873b191508519a62c05
strawhat404 <yosephtesfaye27@gmail.com>
2025-11-22 23:33:01 +0300

    Feat(model creating scanner): in scanner app model created

--- backend/scanner/models.py
+++ backend/scanner/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class ScanTask(models.Model):
+    url = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.url
 # Create your models here.
