c92fc048bc31abfb6c5f63dcf9945b4865af97a6
strawhat404 <yosephtesfaye27@gmail.com>
2025-11-23 13:02:57 +0300

    Fix(urls) : urls of the app and the project configurations

---
+++ backend/scanner/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import ScanResultListCreateView, ScanResultDetailView
+
+urlpatterns = [
+    path('scans/', ScanResultListCreateView.as_view(), name='scan-list-create'),
+    path('scans/<int:pk>/',ScanResultDetailView.as_view(),name='scan_detail'),
+]
