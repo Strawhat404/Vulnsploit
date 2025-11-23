487e4b67a9f22eb60bd1ffac6f8dc0f12854374c
strawhat404 <yosephtesfaye27@gmail.com>
2025-11-23 12:41:32 +0300

    Feat(APIView): APIVIew request handling and response

--- backend/scanner/views.py
+++ backend/scanner/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import ScanResult
+from .serializers import ScanResultSerializer
 # Create your views here.
+
+#List all scan results OR create a new scan result
+
+class ScanResultListCreateView(generics.ListCreateAPIView):
+    queryset = ScanResult.objects.all().order_by('-created_at')
+    serializer_class = ScanResultSerializer
+
+
+#retrieve a single scan result by ID
+
+class ScanResultDetailView(generics.RetrieveAPIView):
+    queryset = ScanResult.objects.all()
+    serializer_class = ScanResultSerializer
