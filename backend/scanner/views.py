ae3eaf0c68882b2acdd9f773e3699c4046e970bc
strawhat404 <yosephtesfaye27@gmail.com>
2025-11-26 20:30:44 +0300

    Fix(view to forward to celery): View forwards tasks to celery instead of scanning in it itself

--- backend/scanner/views.py
+++ backend/scanner/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import ScanResult
 from .serializers import ScanResultSerializer
-from .utils import run_nmap
+from .utils import run_scan
 
 class ScanResultListCreateView(generics.ListCreateAPIView):
     queryset = ScanResult.objects.all().order_by('-created_at')
@@ -13,9 +13,10 @@ class ScanResultListCreateView(generics.ListCreateAPIView):
         scan_type = self.request.data.get("scan_type")
 
         # Call our utility function that executes Nmap inside Docker
-        output = run_nmap(target, scan_type)
+       scan_instances =  serializer.save(result="Scan in progress")
+       execute_scan.delay(scan_instances.id,target,scan_type)
 
-        serializer.save(result=output)
+       
 
 class ScanResultDetailView(generics.RetrieveAPIView):
     queryset = ScanResult.objects.all()
